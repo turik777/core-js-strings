40d27acec896c58e718999cb5d1e96c072ec4baa
turik777 <52492191+turik777@users.noreply.github.com>
2024-11-03 04:01:02 +0300

    feat: complete countVowels function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -291,8 +291,10 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+  return str.split('').filter((char) => vowels.includes(char.toLowerCase()))
+    .length;
 }
 
 /**
